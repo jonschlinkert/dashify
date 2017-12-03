260579e9dad5972ead54e9fa20fb95162c05c713
jonschlinkert <jon.schlinkert@sellside.com>
2017-12-03 17:55:21 -0500

    run update

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*!
  * dashify <https://github.com/jonschlinkert/dashify>
  *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, 2017, Jon Schlinkert.
+ * Released under the MIT License.
  */
 
 'use strict';
