874998dcd80fa9d61780012866549dc9eb73660c
Adam Trager <hello@adamtrager.com>
2021-09-03 11:38:35 -0400

    Add option to split on digit strings
    Had a need for this use case, thought it might be valuable to others

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = (str, options) => {
   if (typeof str !== 'string') throw new TypeError('expected a string');
   return str.trim()
     .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .replace(/(\d+)/g, m => options && options.splitOnDigits ? `-${m}-` : m)
     .replace(/\W/g, m => /[À-ž]/.test(m) ? m : '-')
     .replace(/^-+|-+$/g, '')
     .replace(/-{2,}/g, m => options && options.condense ? '-' : m)
